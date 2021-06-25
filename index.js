541836f5a7fd90caf7e781daa7a196f490467a43
Adion52330 <85357798+Adion52330@users.noreply.github.com>
2021-06-25 14:15:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+if (document.getElementById("branding").innerHTML == "| Welcome to Adion |") {
+  setInterval(function () {
+    document.getElementById("branding").innerHTML = "| Let's Code Faster |";
+  }, 3000);
+}
+setInterval(function () {
+  document.getElementById("branding").innerHTML = "| Welcome To Adion |";
+}, 6000);
